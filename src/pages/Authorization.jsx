b3c6ef266c50e3324df2cd06a2965c98184c90cf
vigneshdevelopr <vigneshtpr99@gmail.com>
2024-02-23 20:57:04 +0530

    update_3

--- src/pages/Authorization.jsx
+++ src/pages/Authorization.jsx
@@ -106,8 +106,8 @@ const AddUser = async (event) => {
         email,
         password,
       };
-      // const response = await fetch("https://imdb-server-7smr.onrender.com/auth/login", {
-      const response = await fetch("http://localhost:5000/auth/login", {
+      const response = await fetch("https://imdb-server-7smr.onrender.com/auth/login", {
+      // const response = await fetch("http://localhost:5000/auth/login", {
         method: "POST",
         body: JSON.stringify(newData),
 
